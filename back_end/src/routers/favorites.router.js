d537791c70dcb7b18a66527d988ed3d0932992b2
ghi3621 <154965590+ghi3621@users.noreply.github.com>
2024-02-15 03:59:05 +0900

    좋아요 기능 일단 최종

--- back_end/src/routers/favorites.router.js
+++ back_end/src/routers/favorites.router.js
@@ -13,39 +13,48 @@ const router = express.Router();
 //2. 좋아요 취소 가능
 
 // 게시글 좋아요
+
 router.post("/posts/:postId/favorites", authMiddleware, async (req, res) => {
   const { postId } = req.params;
   const { userId } = res.locals.user;
   try {
-    const existingLike = await prisma.favorites.findFirst({
+    const existingFav = await prisma.posts.findFirst({
       where: {
         postId: +postId,
       },
-      select: { userId: true },
     });
-    if (!existingLike) {
+    const favoritesPosts = await prisma.favorites.findFirst({
+      where: {
+        postId: +postId,
+        userId: +userId,
+      },
+    });
+
+    if (!existingFav) {
       return res.status(400).json({
         errorMessage: "게시물이 존재하지 않습니다. 확인해주세요!!",
       });
     }
 
-    // 토큰 유저의 정보가 해당 게시물을 좋아요.
-    // 처음이라면 '좋아요' message: 게시물 좋아요 성공!!
-    const favoritesPosts = await prisma.$transaction([
-      prisma.favorites.create({
+    if (!favoritesPosts) {
+      await prisma.favorites.create({
         data: {
           postId: +postId,
-          userId: userId,
+          userId: +userId,
         },
-      }),
-      prisma.posts.update({
+      });
+      await prisma.posts.update({
         where: { postId: +postId },
         data: { fav_cnt: { increment: 1 } },
-      }),
-    ]);
-    return res
-      .status(201)
-      .json({ message: "게시물 좋아요 성공 (^O^)", favoritesPosts });
+      });
+    } else {
+      return res.status(400).json({
+        errorMessage: "이미 좋아요 했습니다. 좋아요는 1번만 가능해요 ㅠㅠ!!",
+      });
+    }
+    return res.status(200).json({
+      message: "게시물 좋아요 성공 (^O^)",
+    });
   } catch (err) {
     next(err);
   }
@@ -53,91 +62,136 @@ router.post("/posts/:postId/favorites", authMiddleware, async (req, res) => {
 
 // 댓글 좋아요
 router.post(
-  "/comment/:commentId/favorites",
+  "/comments/:commentId/favorites",
   authMiddleware,
   async (req, res) => {
     const { commentId } = req.params;
-    const { userId } = req.user;
+    const { userId } = res.locals.user;
     try {
-      const existingLike = await prisma.favorites.findFirst({
+      const existingFav = await prisma.comments.findFirst({
         where: {
           commentId: +commentId,
-          userId: userId,
         },
       });
-      if (!existingLike) {
+      const favoritesComments = await prisma.favorites.findFirst({
+        where: {
+          commentId: +commentId,
+          userId: +userId,
+        },
+      });
+
+      if (!existingFav) {
+        return res.status(400).json({
+          errorMessage: "댓글이 존재하지 않습니다. 확인해주세요!!",
+        });
+      }
+
+      if (!favoritesComments) {
+        await prisma.favorites.create({
+          data: {
+            commentId: +commentId,
+            userId: +userId,
+          },
+        });
+        await prisma.comments.update({
+          where: { commentId: +commentId },
+          data: { click: { increment: 1 } },
+        });
+      } else {
         return res.status(400).json({
           errorMessage: "이미 좋아요 했습니다. 좋아요는 1번만 가능해요 ㅠㅠ!!",
         });
       }
-
-      // 토큰 유저의 정보가 해당 게시물을 좋아요.
-      // 처음이라면 '좋아요' message: 게시물 좋아요 성공!!
-      const favoritesPosts = await prisma.$transaction([
-        prisma.favorites.create({
-          data: {
-            commentId: +commentId,
-            userId: userId,
-          },
-        }),
-        prisma.comments.update({
-          where: { commentId: +commentId },
-          data: { click: { increment: 1 } },
-        }),
-      ]);
-      return res
-        .status(201)
-        .json({ message: "게시물 좋아요 성공 (^O^)", favoritesPosts });
+      return res.status(200).json({
+        message: "댓글 좋아요 성공 (^O^)",
+      });
     } catch (err) {
       next(err);
     }
   }
 );
 
-//좋아요 수정
+//수정
+//게시물 좋아요 수정
 router.put("/posts/:postId/favorites", authMiddleware, async (req, res) => {
   const { userId } = res.locals.user;
   const { postId } = req.params;
   // const { fav_cnt } = req.body;
   try {
-    const existingLike = await prisma.posts.findFirst({
+    const existingFav = await prisma.posts.findFirst({
       where: {
         postId: +postId,
       },
     });
 
-    if (!existingLike) {
+    if (!existingFav) {
       return res
         .status(400)
         .json({ errorMessage: "게시물이 존재하지 않습니다. 확인해주세요!!" });
     }
 
-    const favoritesPosts = await prisma.$transaction([
-      prisma.favorites.findFirst({
-        where: {
-          postId: +postId,
-          userId: userId,
-        },
-      }),
-      prisma.posts.update({
+    const favoritesPosts = await prisma.favorites.findFirst({
+      where: {
+        postId: +postId,
+        userId: +userId,
+      },
+    });
+
+    if (favoritesPosts) {
+      await prisma.posts.update({
         where: { postId: +postId },
         data: { fav_cnt: { decrement: 1 } },
-      }),
-    ]);
-    // if (favoritesPosts) {
-    //   prisma.favorites.update({
-    //     where: { postId: +postId },
-    //     data: { fav_cnt: { decrement: 1 } },
-    //   });
-    // } else {
-
-    // if (return res.status(400).json({ errorMessage: "좋아요가 없습니다." });
-    return res
-      .status(201)
-      .json({ message: "좋아요 취소 (ㅠOㅠ)", favoritesPosts });
+      });
+    } else {
+      return res.status(400).json({ errorMessage: "좋아요가 없습니다!" });
+    }
+    return res.status(201).json({ message: "좋아요 취소 (ㅠOㅠ)" });
   } catch (error) {
     next(err);
   }
 });
+
+// 댓글 좋아요 수정
+router.put(
+  "/comments/:commentId/favorites",
+  authMiddleware,
+  async (req, res) => {
+    const { userId } = res.locals.user;
+    const { commentId } = req.params;
+    // const { fav_cnt } = req.body;
+    try {
+      const existingFav = await prisma.comments.findFirst({
+        where: {
+          commentId: +commentId,
+        },
+      });
+
+      if (!existingFav) {
+        return res
+          .status(400)
+          .json({ errorMessage: "게시물이 존재하지 않습니다. 확인해주세요!!" });
+      }
+
+      const favoritesComments = await prisma.favorites.findFirst({
+        where: {
+          commentId: +commentId,
+          userId: +userId,
+        },
+      });
+
+      if (favoritesComments) {
+        await prisma.comments.update({
+          where: { commentId: +commentId },
+          data: { click: { decrement: 1 } },
+        });
+      } else {
+        return res.status(400).json({ errorMessage: "좋아요가 없습니다!" });
+      }
+      return res.status(201).json({ message: "좋아요 취소 (ㅠOㅠ)" });
+    } catch (error) {
+      next(err);
+    }
+  }
+);
 
 export default router;
